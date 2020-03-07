9c41342c15107f3c94e90ded0569e7afca5b421c
armezo <sagar.tripathi@armezo.com>
2020-03-07 13:10:09 +0530

    --- Auto-Git Commit ---

--- src/app/lesson-modal/lesson-modal.component.ts
+++ src/app/lesson-modal/lesson-modal.component.ts
@@ -4,6 +4,7 @@ import { MatDialog} from '@angular/material';
 import { ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-lesson-modal',
@@ -17,9 +18,34 @@ export class LessonModalComponent implements OnInit {
   ngOnInit() {
     
   }
+  todo = [
+    'Email ID',
+    'Phone Number',
+    'Option1',
+    'Option2',
+    'Option3',
+    'Option4',
+    'Option5',
+  ];
 
-  closeModal(){
-    this.dialog.closeAll();
-  }
+  done = [
+    'Full Name',
+    'Date of Birth'
+  ];
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+
+  // closeModal(){
+  //   this.dialog.closeAll();
+  // }
+
+}
 }
