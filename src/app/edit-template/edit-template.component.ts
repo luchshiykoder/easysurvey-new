5bd83ea1848c4b7fa42561967c552c9a9ffa7e0b
armezo <sagar.tripathi@armezo.com>
2020-03-12 11:24:43 +0530

    --- Auto-Git Commit ---

--- src/app/edit-template/edit-template.component.ts
+++ src/app/edit-template/edit-template.component.ts
@@ -348,4 +348,23 @@ selectedUserTab = 1;
 //   copy() {
 //     this.movies.push(this.movies.length + 1)
 //  }
+private selectedLink: string="Male";        
+  
+  setradio(e: string): void   
+  {  
+  
+    this.selectedLink = e;  
+          
+  }  
+  
+    isSelected(name: string): boolean   
+  {  
+  
+        if (!this.selectedLink) { // if no radio button is selected, always return false so every nothing is shown  
+            return false;  
+  }  
+  
+        return (this.selectedLink === name); // if current radio button is selected, return true, else return false  
+    }  
+  
 }
