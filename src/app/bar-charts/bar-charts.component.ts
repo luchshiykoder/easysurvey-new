1b0c866cc9070c7a682800909a18e81fe9ea0c96
armezo <sagar.tripathi@armezo.com>
2020-03-17 18:37:50 +0530

    --- Auto-Git Commit ---

--- src/app/bar-charts/bar-charts.component.ts
+++ src/app/bar-charts/bar-charts.component.ts
@@ -18,20 +18,20 @@ export class BarChartsComponent implements OnInit {
   };
   
   //for barChartData
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType = 'bar';
-  public barChartLegend = true;
+  // public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  // public barChartType = 'bar';
+  // public barChartLegend = true;
 
-  public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  ];
+  // public barChartData = [
+  //   {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+  //   {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  // ];
 
 
   //for doughnut
-  public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
-  public doughnutChartData = [120, 150, 180, 90];
-  public doughnutChartType = 'doughnut';
+  // public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  // public doughnutChartData = [120, 150, 180, 90];
+  // public doughnutChartType = 'doughnut';
   ngOnInit() {
   }
 
