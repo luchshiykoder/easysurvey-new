21f0998b3a3137145609d2e5a8c59703c9c6d115
armezo <sagar.tripathi@armezo.com>
2020-03-17 17:37:38 +0530

    --- Auto-Git Commit ---

--- src/app/bar-charts/bar-charts.component.ts
+++ src/app/bar-charts/bar-charts.component.ts
@@ -6,13 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bar-charts.component.css']
 })
 export class BarChartsComponent implements OnInit {
-
-  public pieChartLabels = ['Pending', 'Attendent', 'Total'];
-  public pieChartData = [1200, 150, 200];
+  //for pie
+  public pieChartLabels = ['Attendent', 'Pending'];
+  public pieChartData = [200, 100];
   public pieChartType = 'pie';
 
   constructor() { }
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  
+  //for barChartData
+  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
 
+  public barChartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
+
+  //for doughnut
+  public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public doughnutChartData = [120, 150, 180, 90];
+  public doughnutChartType = 'doughnut';
   ngOnInit() {
   }
 
